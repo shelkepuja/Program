6c412befaa40397ea024135d4093e8415f35dc7a
Shelake Pooja <74140442+shelkepuja@users.noreply.github.com>
2020-11-08 18:58:47 +0530

    Update main.c

--- Program In C/main.c
+++ Program In C/main.c
@@ -1,5 +1,14 @@
 /*
 Problem statement: Accept number from user and Check whether number is even or odd.
+
+Input:Enter Number
+      10
+Output:Number is Even
+
+Input:Enter Number
+      15
+Output:Number is Odd
+
 */
 #include <stdio.h>
 
@@ -11,11 +20,11 @@ typedef int BOOL;
 BOOL ChkEvnOdd(int iNo)
 {
 
-int iResult=0;
+int iResult=0;// Local varible
 
 iResult=iNo%2;
 
-if(iResult==0)
+if(iResult==0)//After division remainder get zero then it will return true.
 {
 return TRUE;
 }
@@ -26,7 +35,7 @@ return FALSE;
 
 }
 
-int main()
+int main()//Entry Point function where execution starts.
 {
 int iValue=0;
 BOOL bAns=FALSE;
@@ -43,8 +52,8 @@ printf("Number is Even\n");
 
 else
 {
-printf("Number is odd\n");
+printf("Number is Odd\n");
 }
 
-return 0;
+return 0;//Successful termination
 }
